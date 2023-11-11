cf2a852d2c4a81fd76f8f2b6520981eff80be722
A.Musienko <72299398+alexmusienko@users.noreply.github.com>
2023-11-11 21:19:56 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+
+    const [valueOnDisplay, setValueOnDisplay] = useState(0);
+    const [currency, setCurrency] = useState('USD');
+
+    const getRate = async (currency) => {
+        const resp = await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${currency}&json`);
+        const data = await resp.json();
+        return data[0].rate;
+    }
+
+    useEffect(() => {
+        getRate(currency)
+            .then(rate => {
+                let resultValue = 'N/A';
+                if (currency && rate) {
+                    resultValue = (props.valueUAH / rate).toFixed(2);
+                };
+                setValueOnDisplay(resultValue);
+            })
+    }, [currency, props.valueUAH]);
+
+    return (
+
+        <div className="app">
+            <div className="tochange">Меняем: {props.valueUAH} грн</div>
+            <div className="counter">{valueOnDisplay}</div>
+            <div className="controls">
+                <button onClick={() => setCurrency('USD')}>USD</button>
+                <button onClick={() => setCurrency('EUR')}>EUR</button>
+                <button onClick={() => setCurrency('PLN')}>PLN</button>
+                <button onClick={() => setCurrency('NOK')}>NOK</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
